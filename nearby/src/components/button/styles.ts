a2a9ca16088c4278e785471dea51a62841f1aac0
Janyson Marques <janyson29@hotmail.com>
2025-11-18 20:00:19 -0300

    add projeto

---
+++ nearby/src/components/button/styles.ts
@@ -0,0 +1,19 @@
+import { colors, fontFamily } from "@/styles/theme";
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: {
+    height: 56,
+    maxHeight: 56,
+    backgroundColor: colors.green.base,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 14,
+  },
+  title: {
+    fontFamily: fontFamily.semiBold,
+    color: colors.gray[100],
+    fontSize: 16,
+    },
+})
---
+++ nearby/src/components/category/index.tsx
@@ -0,0 +1,24 @@
+import { Pressable, PressableProps, Text } from "react-native";
+
+import { colors } from "@/styles/theme";
+import { categoriesIcons } from "@/utils/categories-icons";
+import { styles } from "./styles";
+
+type Props = PressableProps & {
+  iconId: string,
+  isSelected: boolean,
+  name: string,
+}
+
+export function Category({name, iconId, isSelected = false, ...rest }: Props) {
+  const Icon = categoriesIcons[iconId];
+  return (
+    <Pressable
+      style={[styles.container, isSelected && styles.containerSelected]}
+      {...rest}
+    >
+      <Icon size={16} color={colors.gray[isSelected ? 100 : 400]}/>
+      <Text style={[styles.name, isSelected && styles.nameSelected]}>{name}</Text>
+    </Pressable>
+  )
+}
